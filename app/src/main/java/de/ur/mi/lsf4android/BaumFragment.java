90417b957cdb4f50f60f758b7ada8fa839640198
suko16 <susanne.korbely96@gmx.de>
2017-05-12 11:08:33 +0200

    Susi Stand 12.05.17 11:18

--- app/src/main/java/de/ur/mi/lsf4android/BaumFragment.java
+++ app/src/main/java/de/ur/mi/lsf4android/BaumFragment.java
@@ -21,7 +21,7 @@ import android.view.ViewGroup;
  * Use the {@link BaumFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BaumFragment extends Fragment {
+public class BaumFragment extends android.support.v4.app.Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
